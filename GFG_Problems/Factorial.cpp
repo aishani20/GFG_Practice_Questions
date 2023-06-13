72950f18ac9dbcdd9241fa7751effd8b23628eba
Aishani Mohapatra <mohapatraaishani20@gmail.com>
2023-06-14 00:28:25 +0530

    File added

---
+++ GFG_Problems/Factorial.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    long long int factorial(int N){
+        //code here
+        if(N==1 || N==0)
+        return 1;
+        
+        return N * factorial(N-1);
+      
+    
+    
+    }
+};
