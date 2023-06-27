60c4ab6569c413ee826dc99d6e3c6789c8ea2b1d
Aishani Mohapatra <mohapatraaishani20@gmail.com>
2023-06-28 01:56:45 +0530

    Updated file

--- GFG_Problems/Binary number to decimal number.cpp
+++ GFG_Problems/Binary number to decimal number.cpp
@@ -14,3 +14,17 @@ class Solution {
     return sum;
 }
 };
+
+//2nd approach
+
+class Solution {
+  public:
+    int binary_to_decimal(string str) {
+        // Code here.
+        int ans=0;
+        for(int i=str.length()-1; i>=0; i--){
+            ans=ans+(str[i]-'0')*pow(2,str.length()-i-1);
+        }
+        return ans;
+    }
+};
