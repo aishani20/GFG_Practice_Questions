3b3fb2ab72ff4ba3acb7d1b7fc38713a6bc5d19d
Aishani Mohapatra <mohapatraaishani20@gmail.com>
2023-07-14 01:53:54 +0530

    Update power set using recursion

--- GFG_Problems/Power Set.cpp
+++ GFG_Problems/Power Set.cpp
@@ -25,3 +25,34 @@ class Solution{
         return ans;
 		}
 };
+
+//APPROACH-2
+
+class Solution{
+	public:
+	void f(string s,string output, int i,vector<string>&ds){
+	    //base case
+	    if(i>=s.length()){
+	        if(output.length()>0)
+	        ds.push_back(output);
+	        return;
+	    }
+	    char element=s[i];
+	    output.push_back(element);
+	    f(s,output,i+1,ds);
+	    
+	    output.pop_back();
+	    f(s,output,i+1,ds);
+	    
+	}
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		    vector<string>ds;
+		    
+		    string output="";
+		    f(s,output,0,ds);
+		    sort(ds.begin(),ds.end());
+		    return ds;
+		
+		}
+};
